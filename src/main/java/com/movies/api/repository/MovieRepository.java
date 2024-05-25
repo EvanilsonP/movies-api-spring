73fb0baab5d4fdfbc719f2682965bf25f888282e
Evanilson Pereira <pevanilson99@gmail.com>
2024-05-25 13:47:53 -0300

    Created movie repository

---
+++ src/main/java/com/movies/api/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.movies.api.repository;
+
+import com.movies.api.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Integer, Movie> {
+}
