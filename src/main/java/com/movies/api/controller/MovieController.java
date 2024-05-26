d71d1d78adaa3cc34f0d6ed12bb0997837c0258b
Evanilson Pereira <pevanilson99@gmail.com>
2024-05-26 13:00:49 -0300

    Created method to get all movies

--- src/main/java/com/movies/api/controller/MovieController.java
+++ src/main/java/com/movies/api/controller/MovieController.java
@@ -5,10 +5,9 @@ import com.movies.api.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -17,9 +16,16 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
+    // Create and save movie to the database
     @PostMapping("/create")
     public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
         Movie newMovie = movieRepository.save(movie);
         return new ResponseEntity<>(newMovie, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieRepository.findAll();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
     }
 }
