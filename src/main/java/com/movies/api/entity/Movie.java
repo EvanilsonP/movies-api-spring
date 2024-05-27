20008efd4713688a90ed63d48f66715f38087419
Evanilson Pereira <pevanilson99@gmail.com>
2024-05-27 14:51:07 -0300

    Used NotBlank and NotNull annotations to prevent empty fields

--- src/main/java/com/movies/api/entity/Movie.java
+++ src/main/java/com/movies/api/entity/Movie.java
@@ -1,6 +1,8 @@
 package com.movies.api.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,21 +19,27 @@ public class Movie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "Title field is mandatory.")
     @Column(name = "movie", unique = true)
     private String title;
 
+    @NotBlank(message = "Description field is mandatory.")
     @Column(name = "description")
     private String description;
 
+    @NotBlank(message = "Genre field is mandatory.")
     @Column(name = "genre")
     private String genre;
 
+    @NotNull(message = "Release date field is mandatory.")
     @Column(name = "release_date")
     private LocalDate releaseDate;
 
+    @NotBlank(message = "The Director field is mandatory.")
     @Column(name = "director")
     private String director;
 
+    @NotNull(message = "The Rating field is mandatory.")
     @Column(name = "rating")
     private Double rating;
 
